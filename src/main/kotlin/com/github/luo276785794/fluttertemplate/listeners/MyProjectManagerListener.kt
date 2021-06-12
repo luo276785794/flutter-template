dd80e95387cf8abc0323fa99a2f4ce482697d5b0
GitHub Action <action@github.com>
2021-06-12 15:28:37 +0000

    Template cleanup

--- src/main/kotlin/com/github/luo276785794/fluttertemplate/listeners/MyProjectManagerListener.kt
+++ src/main/kotlin/com/github/luo276785794/fluttertemplate/listeners/MyProjectManagerListener.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.luo276785794.fluttertemplate.listeners
 
+import com.github.luo276785794.fluttertemplate.services.MyProjectService
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManagerListener
-import org.jetbrains.plugins.template.services.MyProjectService
 
 internal class MyProjectManagerListener : ProjectManagerListener {
 
---
+++ src/main/kotlin/com/github/luo276785794/fluttertemplate/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.luo276785794.fluttertemplate.services
+
+import com.github.luo276785794.fluttertemplate.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
